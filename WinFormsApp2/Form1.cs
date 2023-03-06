fb2b796ecd777e7ec6e01b547110cb91aadec6f1
student <student@LAB-PAR-14>
2023-03-06 09:27:15 +0100

    Dodaj pliki projektów.

---
+++ WinFormsApp2/Form1.cs
@@ -0,0 +1,23 @@
+namespace WinFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Text = DateTime.Now.ToString("HH:mm:ss");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form form = new Form();
+            form.ShowDialog();  
+        }
+    }
+}
